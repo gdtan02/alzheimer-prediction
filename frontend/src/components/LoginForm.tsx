f4dc4cc43cec3c66d2df9f9212349f86943a785f
tgd1115 <gdtan021115@gmail.com>
2025-04-09 00:35:53 +0800

    basic configuration and add ShadCN UI components

---
+++ frontend/src/components/LoginForm.tsx
@@ -0,0 +1,18 @@
+// import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+// import { auth } from "@/config/firebase";
+
+// import { z } from "zod";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useForm } from "react-hook-form";
+
+// import { Button } from "./ui/button";
+// import { Form, FormControl, FormDescription, FormField, FormItem, FormMessage } from "./ui/form";
+
+const LoginForm = () => {
+    return (<></>)
+};
+
+export default LoginForm;
---
+++ frontend/src/components/RegisterForm.tsx
@@ -0,0 +1,17 @@
+// import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { z } from "zod";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useForm } from "react-hook-form";
+// import { toast } from "sonner";
+
+// import { Form, FormControl, FormDescription, FormField, FormMessage, FormItem, FormLabel } from "./ui/form";
+// import { Button } from "./ui/button";
+// import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+// import { Input } from "./ui/input";
+
+const RegisterForm = () => {
+    return (<></>)
+};
+
+export default RegisterForm;
